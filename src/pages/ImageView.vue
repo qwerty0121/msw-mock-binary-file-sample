c9d35b7da39d7b487e507f9e5bc57f5867a0d062
qwerty0121 <sh1ng0718o@gmail.com>
2024-08-24 16:51:14 +0900

    画像表示画面の作成

---
+++ src/pages/ImageView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+
+const imageUrl = ref();
+
+fetch("/api/image/1")
+  .then((response) => response.blob())
+  .then((blob) => (imageUrl.value = URL.createObjectURL(blob)));
+</script>
+
+<template>
+  <div v-if="!imageUrl">Now Loading ...</div>
+  <img v-else class="image-preview" :src="imageUrl"></img>
+</template>
+
+<style scoped>
+.image-preview {
+  width: 100%;
+}
+</style>
