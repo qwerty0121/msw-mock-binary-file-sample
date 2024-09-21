589f29caa0e356fe05f3cd34c6c729a1f3b2c572
qwerty0121 <sh1ng0718o@gmail.com>
2024-09-21 17:12:20 +0900

    refactor: 不要な記述を削除

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,14 +1,12 @@
 import { http, HttpResponse } from "msw";
-import IMAGE_1 from "./images/image-1.jpg?url";
+import IMAGE_1 from "./images/image-1.jpg";
 
 export const handlers = [
-  http.get("/api/image/:id", async ({ params }) => {
+  http.get("/api/image/:id", async () => {
     const imageArrayBuffer = await fetch(IMAGE_1).then((response) =>
       response.arrayBuffer()
     );
 
-    return HttpResponse.arrayBuffer(imageArrayBuffer, {
-      headers: { "Content-Type": "image/jpeg" },
-    });
+    return HttpResponse.arrayBuffer(imageArrayBuffer);
   }),
 ];
